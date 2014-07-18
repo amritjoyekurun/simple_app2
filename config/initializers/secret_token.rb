7ec1aac84bf0169a3b0395b5c0a0576d0e34d068
Amrit Joyekurun <info@katizo.com>
2014-07-18 01:35:34 -0400

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WebBlog::Application.config.secret_key_base = 'cd617c9ca546637d0f30a45abf0a154799e33470c6b9c0ec5b9992135673c41359617577e409b2697125e6165323f320098d2b9f1eec99d723be1b0212194c62'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WebBlog::Application.config.session_store :cookie_store, key: '_web_blog_session'
